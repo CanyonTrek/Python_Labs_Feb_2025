17b40bdb5b248100981d14f4b022f5d15f240b79
Brad Ruttan <BradRuttan@comcast.net>
2024-11-18 21:00:36 +0000

    Created First.py

--- First.py
+++ First.py
@@ -9,7 +9,7 @@ print("my name is:", name) #Comma does stuff
 print("My name is " + name)
 
 import random
-print(My lucky number is", random.randint(1,50))
+print("My lucky number is", random.randint(1,50))
 
 import math
-print("Cosine of 0.5 is" math.cos(0.5))
+print("Cosine of 0.5 is", math.cos(0.5))
