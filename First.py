7e958efd3dc4d2f65cf9cac50ad038f549a9d937
Brad Ruttan <BradRuttan@comcast.net>
2024-11-18 20:57:18 +0000

    Created first.py

---
+++ First.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+#Author: Brad
+#version: 1.0
+#Description: First python script
+
+name = input ("Enter your name: ")
+
+print("my name is:", name) #Comma does stuff
+print("My name is " + name)
+
+import random
+print(My lucky number is", random.randint(1,50))
+
+import math
+print("Cosine of 0.5 is" math.cos(0.5))
