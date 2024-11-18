231aec5ff46be4559152b535ac3d62aec52a2eac
Brad Ruttan <BradRuttan@comcast.net>
2024-11-18 22:47:41 +0000

    EX1.py added

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+#Author: Brad
+#version: 1.0
+#Description: Ex1 python script
+
+firstname = input ("Enter your First Name: ")
+lastname = input ("Enter your Last Name: ")
+
+print("Your name is:" ,firstname, lastname)
