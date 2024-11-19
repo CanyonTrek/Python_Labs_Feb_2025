16daf55e11b62acc1eac4bc4afe8d2e611cabde1
Brad Ruttan <BradRuttan@comcast.net>
2024-11-19 23:40:10 +0000

    Belgium_SEP.py

---
+++ demo_str_format.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# Author: Brad
+# version: 1.0
+# Description: f-strings Script Info
+
+"""
+    Docstring:
+"""
+
--- labs/sep.py
+++ labs/sep.py
@@ -1,4 +1,19 @@
-#! /usr/bin/python
+#! /usr/bin/env python3
+# Author: Brad
+# version: 1.0
+# Description: f-strings Script Info
+
+
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
+
+print("-" * len(Belgium))
+print(Belgium.replace(",", ":"))
+
+pop_country = int(Belgium[8:16])
+pop_capital = int(Belgium[26:32])
+
+print(f"Population of Country and Capital = {pop_country + pop_capital}")
+print("-" * len(Belgium))
+
 
