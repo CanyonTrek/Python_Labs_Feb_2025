8817adaebe1c1659738910ecd02f20e1736c4df9
Brad Ruttan <BradRuttan@comcast.net>
2024-11-19 20:39:58 +0000

    Demo getpass

--- demo_pin.py
+++ demo_pin.py
@@ -8,12 +8,16 @@
 DocString
 """
 
+import getpass
+
+
+
 master_pin = "0123"
 pin = None
 attempts = 0
 
 while pin != master_pin and attempts <3:
-    pin = input("Enter PIN: ")
+    pin = getpass.getpass("Enter PIN: ")
     if pin ==master_pin:
         print("Valid PIN")
         break
