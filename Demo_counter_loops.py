c3df3aa2a223d348c9ed4b65192441a511bf90d6
Brad Ruttan <BradRuttan@comcast.net>
2024-11-19 19:38:33 +0000

    demo for step in range

---
+++ Demo_counter_loops.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+# Author: Brad
+# version: 1.0
+# Description: THis Script will demo how to repeat a block of commands a specific number sof times.
+
+
+"""
+    Docstring:
+"""
+
+count = 0 # initial counter
+while count <10:  # loop condition
+    print(count)
+    count += 1  # counter
+
+# range ( start, stop, step )
+for num in range(0, 10, 1):
+    print(num)
+
