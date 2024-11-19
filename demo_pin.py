e7d9a9fb5502c69e682b2b14f370e5b081747919
Brad Ruttan <BradRuttan@comcast.net>
2024-11-19 19:30:20 +0000

    created and updated Demo_pin.py

--- demo_pin.py
+++ demo_pin.py
@@ -16,8 +16,14 @@ while pin != master_pin and attempts <3:
     pin = input("Enter PIN: ")
     if pin ==master_pin:
         print("Valid PIN")
+        break
     else:
         print("Invalid PIN")
         attempts +=1
+
+else:
+    # Executes only once when conditions False.
+    print("Too many attempts")
+    print("Your card has been retained. Have a nice day!")
 
 print("Done. ")
