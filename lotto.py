2467ea71f5fe2f8bd8bab09f43188f8edce4dfc9
Brad Ruttan <BradRuttan@comcast.net>
2024-11-19 19:08:45 +0000

    created and updated lotto.py

--- lotto.py
+++ lotto.py
@@ -9,9 +9,11 @@ import random
 lotto = [] # create an empty list
 
 while len(lotto) < 6:
-    num = random.randit( 1, 50)
+    num = random.randint( 1, 50)
     if num not in lotto:
         lotto.append(num)
     else:
-        print("Duplicate number: ", num
-print("Lottery numbers =", lotto
+        print("Duplicate number: ", num)
+
+
+print("Lottery numbers =", lotto)
