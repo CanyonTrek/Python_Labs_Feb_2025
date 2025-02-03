0154a31df1c94054708b07de73de6623a82cd92e
unknown <jorge.lopez.mx@gmail.com>
2025-02-03 23:53:57 +0000

    Update files

--- first.py
+++ first.py
@@ -4,13 +4,16 @@
 # Description: This is oir first script
 
 name = input("Enter your name: ")
+number = input("Enter the number : ")
 
 print("My name is", name)
 print("My name is " + name)
+print("The number is: ", type(number))
 
 import random
 print("Lucky number is", random.randint(1, 50))
 
 from math import cos
 print("Cosine of 0.5 is", cos(0.5))
+
 
