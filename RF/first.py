4ce380b4158e9b237cfbda6325646e74b2acb055
Roberto Furlan <Roberto.furlan@oracle.com>
2025-02-03 22:55:15 +0000

    Added first.py

---
+++ RF/first.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# author: RF
+# version: 1.0
+# description: first script
+
+# name = "RF"
+name = input("enter your name: ")
+
+print("my name is", name)
+print("my name is " + name)
+
+import random
+print("lucky number is", random.randint(1,50))
+
+import math
+print("cos of 0.5 is", math.cos(0.5))
+
+
