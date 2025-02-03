d5098878b8dc00c93052ca4e3bfd841819a01914
unknown <jorge.lopez.mx@gmail.com>
2025-02-03 22:53:17 +0000

    Add first.py

---
+++ first.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# Author: BersuitX
+# Version: 1.0
+# Description: This is oir first script
+
+name = input("Enter your name: ")
+
+print("My name is", name)
+print("My name is " + name)
+
+import random
+print("Lucky number is", random.randint(1, 50))
+
+from math import cos
+print("Cosine of 0.5 is", cos(0.5))
+
