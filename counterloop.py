ad473c574bf6069187a3c37352a9ad3179caaccc
Roberto Furlan <Roberto.furlan@oracle.com>
2025-02-04 20:20:39 +0000

    added demoplatform.py

---
+++ counterloop.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# Author: Roberto F
+# Description: This script will demo
+"""
+    Docstring:
+"""
+count=0
+while count<10:
+    print(count)
+    count +=1
+
+for num in range(0,10,1):
+    print(num)
--- demo_pin.py
+++ demo_pin.py
@@ -7,13 +7,18 @@
 """
 master_pin = "0123"
 pin = None
+attempts = 0
 
 # loop whilst pin is incorrect
-while pin!=master_pin:
+while pin!=master_pin and attempts < 3:
     pin = input("Enter pin: ")
     if pin==master_pin:
         print("valid pin")
     else:
+        attempts+=1
         print("invalid pin")
+
+if attempts==3:
+    print("tried too many times, call your bank")
 
 print("done.")
