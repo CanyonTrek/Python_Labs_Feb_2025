5b202b20246fd37cf19fa4bb893387b2c41c60e3
Roberto Furlan <Roberto.furlan@oracle.com>
2025-02-04 01:25:28 +0000

    Added ex2.py

---
+++ RF/ex2.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+# author: RF
+# version: 1.0
+# description: 2nd script
+
+name1 = "Roberto"
+name2 = "Furlan"
+print(name1, name2)
+
+nameList = [name1, name2]
+print(nameList)
+
+nameDict = {'first': name1, 'second': name2}
+print(nameDict)
+
---
+++ RF/ex2_2.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# author: RF
+# version: 1.0
+# description: 2nd script (_2)
+
+var = input("please enter a value:")
+print(var.upper())
+
+print(len(str(var)))
+
+print(var.isdecimal())
+
+
