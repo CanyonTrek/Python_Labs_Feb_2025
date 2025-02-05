394dd78f274697348335aee1a0c4bc4f2a2c69b0
Admin <roberto.furlan@oracle.com>
2025-02-05 20:28:28 +0000

    do not want

--- demo_regex.py
+++ demo_regex.py
@@ -4,9 +4,18 @@
 """
     Docstring:
 """
+
+import re
+
 fh_in = open(r"c:\labs\words", mode="rt")
 
 for line in fh_in:
-    if line.startswith("Y") and line.rstrip("\n").endswith("n") and "town" in line:
+    #if line.startswith("Y") and line.rstrip("\n").endswith("n") and "town" in line:
+    #m = re.search("the", line) #the in any position
+    #m = re.search("^the", line) #beginning with the
+    #m = re.search("ing$", line)  # ending with ing
+    m = re.search("^ring$", line)  # exact match
+
+    if m:
         print(line, end="")
 
