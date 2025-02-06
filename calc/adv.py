c35268c50478f5240d86e23aa9a0aa96d4f9a0b4
Admin <roberto.furlan@oracle.com>
2025-02-06 18:58:45 +0000

    do not want

---
+++ calc/adv.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+# Author: Roberto F
+# Description: This script will demo
+"""
+    Docstring:
+"""
+
+import sys
+
+def mod(x, z):
+    return x % z
+
+def power(x, z):
+    return x**z
+
+def sqrt(x):
+    return round(x**0.5, 2)
+
+print("Adv Calc app")
+print(f"100 mod 30 = {mod(100,30)}")
+print(f"100^30 = {power(100,30)}")
+print(f"\N{square root} 100  = {sqrt(100)}")
+
+sys.exit(0)
+
